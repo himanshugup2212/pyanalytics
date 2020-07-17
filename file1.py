218ce4a5077c95a9bf7a0883cda79427c27b29ab
himanshugup2212 <himanshu221296@gmail.com>
2020-07-17 23:38:02 +0530

    Today's Changes

---
+++ file1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 17 23:29:17 2020
+
+@author: Himanshu Gupta
+"""
+
+
+#List - ordered collection of items, mutable : [square ]
+list1 = [1,2,3,4,5,'a','Dhiraj',"Upadhyaya",True]  #list type of object with data
+list1  #print when through spyder
+type(list1)  #type of object
+print(list1)  #print when running complete file
